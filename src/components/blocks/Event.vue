90dffb3e46eb91415eac3f7690f61f45bd862e91
Tatiana Androsova <androsovatati@mail.ru>
2018-12-08 12:19:09 +0300

    maps

---
+++ src/components/blocks/Event.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+  .event
+    .event__main-info
+      .event__header.event-header
+        .event-header__user.user
+          .user__avatar
+          .user__name
+          .user__date
+        .event-header__contract.contract
+          .contract__icon
+          .contract__number
+          .contract__link
+      .event__title
+      .event__status
+      .event__invalid-type
+      .event__footer.event-footer
+        .event-footer__files.files
+        .event-footer__show-button
+    .event__attends.attends
+      .attends__comment.comment
+        .comment__title.comment-title
+          .comment-title__icon
+          .comment-title__text
+        .comment__text
+      .attends__photo.photo
+        .photo__title.photo-title
+          .photo-title__icon
+          .photo-title__text
+        .photo__main
+        .photo__additional
+      .attends__record.record
+        .record__title.record-title
+          .record-title__icon
+          .record-title__text
+        .record#waveform
+</template>
+
+<script>
+export default {
+  name: "Event",
+  props: {
+
+  },
+  data() {
+    return {
+      isCollapsed: true,
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/colors.scss";
+
+.event {
+}
+</style>
--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,9 +1,47 @@
 <template lang="pug">
-  .map map
+  .map
+    #map(style="width: 100%; height: 550px;")
 </template>
 
 <script>
+import "here-js-api/scripts/mapsjs-core";
+import "here-js-api/scripts/mapsjs-service";
+import "here-js-api/scripts/mapsjs-ui";
+import "here-js-api/scripts/mapsjs-mapevents";
+import "here-js-api/scripts/mapsjs-clustering";
+
 export default {
-  name: 'Map',
-}
+  name: "Map",
+  created() {},
+  mounted() {
+    const platform = new H.service.Platform({
+      app_id: "WrRxwrtpbzvxLHrOpHyw",
+      app_code: "piJRCl5pH_GmE9birwhupg",
+      ml: "rus",
+      useHTTPS: true
+    });
+
+    const pixelRatio = window.devicePixelRatio || 1;
+    const defaultLayers = platform.createDefaultLayers({
+      tileSize: pixelRatio === 1 ? 256 : 512,
+      ppi: pixelRatio === 1 ? undefined : 320,
+      lg: 'RUS',
+    });
+
+    const map = new H.Map(
+      document.getElementById("map"),
+      defaultLayers.normal.map,
+      {
+        center: new H.geo.Point(55.75370903771494, 37.619813382625585),
+        zoom: 6,
+        pixelRatio: pixelRatio
+      }
+    );
+    const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+    const ui = H.ui.UI.createDefault(map, defaultLayers);
+
+    map.setCenter({ lat: 55.75370903771494, lng: 37.619813382625585 });
+    map.setZoom(10);
+  }
+};
 </script>
