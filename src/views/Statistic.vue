abfa36239b1c0954fd6fedf96905c24a7213b08b
Tatiana Androsova <androsovatati@mail.ru>
2018-12-10 10:26:31 +0300

    heatmap

--- src/views/Statistic.vue
+++ src/views/Statistic.vue
@@ -15,11 +15,16 @@ import "here-js-api/scripts/mapsjs-ui";
 import "here-js-api/scripts/mapsjs-mapevents";
 import "here-js-api/scripts/mapsjs-clustering";
 import "here-js-api/styles/mapsjs-ui.css";
+import "@/lib/here-data.js";
+import { mapState, mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'Statistic',
   components: {
     BaseButton: Button,
+  },
+  computed: {
+    ...mapGetters(['contracts']),
   },
   mounted() {
     const platform = new H.service.Platform({
@@ -31,28 +36,53 @@ export default {
 
     const pixelRatio = window.devicePixelRatio || 1;
     const defaultLayers = platform.createDefaultLayers({
-      tileSize: pixelRatio === 1 ? 256 : 512,
-      ppi: pixelRatio === 1 ? undefined : 320,
-      lg: "RUS"
+    tileSize: pixelRatio === 1 ? 256 : 512,
+    ppi: pixelRatio === 1 ? undefined : 320
     });
-
-    this.$refs.map.addEventListener('DOMNodeInserted', () => {
-      clearTimeout(this.$options.timer);
-      this.$options.timer = setTimeout(() => {
-        this.isLoading = false;
-      }, 100);
-    });
-
-    const map = new H.Map(
-      this.$refs.map,
-      defaultLayers.normal.map,
-      {
-        center: new H.geo.Point(55.75370903771494, 37.619813382625585),
-        zoom: 6,
-        pixelRatio: pixelRatio
-      }
-    );
+    const map = new H.Map(this.$refs.map,
+    defaultLayers.normal.basenight, {pixelRatio: pixelRatio});
     const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+    // this.$refs.map.addEventListener('DOMNodeInserted', () => {
+    //   clearTimeout(this.$options.timer);
+    //   this.$options.timer = setTimeout(() => {
+    //     this.isLoading = false;
+    //   }, 100);
+    // });
+
+    // map.setCenter({ lat: 55.75370903771494, lng: 37.619813382625585 });
+    // map.setZoom(10);
+
+    console.log(H);
+
+    const heatmapProvider = new H.data.heatmap.Provider({
+        colors: new H.data.heatmap.Colors({
+        '0': 'blue',
+        '0.5': 'yellow',
+        '1': 'red'
+        }, true),
+        assumeValues: true
+    });
+
+    const markers = [];
+    this.contracts.forEach(contract => {
+        markers.push(
+          new H.map.Marker(
+            {
+              lat: parseFloat(contract.latitude),
+              lng: parseFloat(contract.longitude)
+            }
+          )
+        );
+    });
+
+    heatmapProvider.addData(markers);
+
+    const heatmapLayer = new H.map.layer.TileLayer(heatmapProvider, {
+      opacity: 0.6
+      });
+    map.addLayer(heatmapLayer);
+
     const ui = H.ui.UI.createDefault(map, defaultLayers);
 
     map.setCenter({ lat: 55.75370903771494, lng: 37.619813382625585 });
